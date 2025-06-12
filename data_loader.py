a243e800c4ef33774c5560908ac714ffba4ee2bf
wasvlad <vladuslav.nechiporuk@gmail.com>
2025-06-12 22:40:50 +0200

    add functionality to download and copy dataset to raw_data.csv

--- data_loader.py
+++ data_loader.py
@@ -1,9 +1,15 @@
 import kagglehub
 import os
-
-os.environ['KAGGLEHUB_CACHE'] = os.path.join(os.getcwd(), "data")
+import shutil
 
 # Download latest version
 path = kagglehub.dataset_download("nelgiriyewithana/emotions")
 
+# Move the data file to ./data/raw_data.csv
+src_file = os.path.join(path, "text.csv")
+dst_file = os.path.join(os.getcwd(), "data", "raw_data.csv")
+os.makedirs(os.path.dirname(dst_file), exist_ok=True)
+shutil.copy2(src_file, dst_file)
+
 print("Path to dataset files:", path)
+print("Raw data copied to:", dst_file)
